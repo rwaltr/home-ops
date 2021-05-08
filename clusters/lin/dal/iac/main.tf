cb3a04e0b6f310d5aa33072d4dd5fb9fdbc9549d
Ryan Walter <rwalt@pm.me>
2021-05-07 20:28:56 -0500

    added boilerplate for DAL cluster
    Signed-off-by: Ryan Walter <rwalt@pm.me>

---
+++ clusters/lin/dal/iac/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+      linode = {
+          source = "linode/linode"
+      }
+      vault = {
+          source = "hashicorp/vault"
+      }
+  }
+}
+
+provider "vault" {
+    address = "https://vault.waltr.tech"
+}
+
+data "vault_generic_secret" "linode" {
+    path = "apikeys/linode"
+}
+
+
+provider "linode" {
+    token = "${data.vault_generic_secret.linode.data["api_token"]}"
+}
+
+# resource "linode_lke_cluster" "dal" {
+#   label = "k8s.dal.waltr.tech"
+#   k8s_version = "1.20"
+#   region = "us-central"
+#   pool {
+#     count = 1
+#     type = "g6-standard-1"
+#   }
+# }
+
+# output "kubeconfig" {
+#   value = base64decode(linode_lke_cluster.dal.kubeconfig)
+#   sensitive = true
+# }
