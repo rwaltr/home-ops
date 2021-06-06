5254b3b29e2dd6971197958801226cd6f5b934ab
Ryan Walter <rwalt@pm.me>
2021-06-06 15:30:36 -0500

    consolidation

--- clusters/lin/dal/iac/main.tf
+++ clusters/lin/dal/iac/main.tf
@@ -1,41 +1,50 @@
 terraform {
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/22827251/terraform/state/lin-dal/"
+    lock_address   = "https://gitlab.com/api/v4/projects/22827251/terraform/state/lin-dal/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/22827251/terraform/state/lin-dal/lock"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    retry_wait_min = 5
+  }
+
   required_providers {
-      linode = {
-          source = "linode/linode"
-      }
-      vault = {
-          source = "hashicorp/vault"
-      }
-      cloudflare = {
-          source = "cloudflare/cloudflare"
-      }
+    linode = {
+      source = "linode/linode"
+    }
+    vault = {
+      source = "hashicorp/vault"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
 
   }
 }
 
 provider "vault" {
-    address = "https://vault.waltr.tech"
+  address = "https://vault.waltr.tech"
 }
 
 data "vault_generic_secret" "linode" {
-    path = "apikeys/linode"
+  path = "apikeys/linode"
 }
 
 data "vault_generic_secret" "cloudflare" {
-    path = "apikeys/cloudflare"
+  path = "apikeys/cloudflare"
 }
 
 
 data "vault_generic_secret" "generic" {
-    path = "apikeys/generic"
+  path = "apikeys/generic"
 }
 
 provider "linode" {
-    token = "${data.vault_generic_secret.linode.data["api_token"]}"
+  token = data.vault_generic_secret.linode.data["api_token"]
 }
 
 provider "cloudflare" {
-    api_token = "${data.vault_generic_secret.cloudflare.data["api_token"]}"
+  api_token = data.vault_generic_secret.cloudflare.data["api_token"]
 }
 
 
