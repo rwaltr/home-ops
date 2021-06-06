e9482d14108e3774df13db1f65f3797d43c6d516
Ryan Walter <rwalt@pm.me>
2021-06-06 15:48:04 -0500

    Adding current state

--- clusters/lin/dal/iac/zomboid.tf
+++ clusters/lin/dal/iac/zomboid.tf
@@ -1,11 +1,11 @@
 
 # Zomboid Server
 resource "linode_instance" "zomboid" {
-  label = "zomboid"
-  image = "linode/fedora34"
-  region = "us-central"
-  type = "g6-standard-2"
-  root_pass = "${data.vault_generic_secret.generic.data["root_pass"]}"
+  label     = "zomboid"
+  image     = "linode/fedora34"
+  region    = "us-central"
+  type      = "g6-standard-2"
+  root_pass = data.vault_generic_secret.generic.data["root_pass"]
 
   provisioner "remote-exec" {
     inline = [
@@ -15,20 +15,20 @@ resource "linode_instance" "zomboid" {
       "mkdir /opt/zomboid/data /opt/zomboid/config -p",
       "podman run -d --network host -v /opt/zomboid/data:/data -v /opt/zomboid/config:/config registry.gitlab.com/rwaltr/container-images/zomboid-server:0.0.1",
       "systemctl stop firewalld"
-   ]
-    connection { 
-        type = "ssh"
-        host = self.ip_address
-        user = "root"
-        password = "${data.vault_generic_secret.generic.data["root_pass"]}"
+    ]
+    connection {
+      type     = "ssh"
+      host     = self.ip_address
+      user     = "root"
+      password = data.vault_generic_secret.generic.data["root_pass"]
     }
   }
 }
 
 resource "cloudflare_record" "zomboid-dns" {
-    name = "zomboid."
-    zone_id = "${data.vault_generic_secret.cloudflare.data["zone_id_rwaltrpro"]}"
-    type = "A"
-    value = linode_instance.zomboid.ip_address
-  
+  name    = "zomboid."
+  zone_id = data.vault_generic_secret.cloudflare.data["zone_id_rwaltrpro"]
+  type    = "A"
+  value   = linode_instance.zomboid.ip_address
+
 }
