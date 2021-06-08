3917b40c0976e56656a085f8ef4b5a402cc0f290
Ryan Walter <rwalt@pm.me>
2021-06-08 12:18:49 -0500

    added test

--- clusters/lin/dal/iac/main.tf
+++ clusters/lin/dal/iac/main.tf
@@ -47,3 +47,9 @@ provider "cloudflare" {
   api_token = data.vault_generic_secret.cloudflare.data["api_token"]
 }
 
+resource "cloudflare_record" "cannary-dns" {
+  name    = "carrary."
+  zone_id = data.vault_generic_secret.cloudflare.data["zone_id_rwaltrpro"]
+  type    = "A"
+  value   = "127.0.0.1"
+}
