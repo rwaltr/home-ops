17154f4a39e4b1c27734b59c22c205e3baa77701
Ryan Walter <rwalt@pm.me>
2021-07-31 17:20:35 -0500

    moved files
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- clusters/lin/main.tf
+++ clusters/lin/main.tf
@@ -1,13 +1,4 @@
 terraform {
-  backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/22827251/terraform/state/lin-dal/"
-    lock_address   = "https://gitlab.com/api/v4/projects/22827251/terraform/state/lin-dal/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/22827251/terraform/state/lin-dal/lock"
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    retry_wait_min = 5
-  }
-
   required_providers {
     linode = {
       source = "linode/linode"
