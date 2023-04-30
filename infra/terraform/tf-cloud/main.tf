134b5687d1c4ed833149cce812f57c4ec5385089
Ryan Walter <rwalt@pm.me>
2023-04-30 17:02:09 -0500

    New TF Workspace
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- infra/terraform/tf-cloud/main.tf
+++ infra/terraform/tf-cloud/main.tf
@@ -27,3 +27,15 @@ resource "tfe_workspace" "linode" {
   organization   = data.tfe_organization.rwaltr.name
   execution_mode = "local"
 }
+
+resource "tfe_workspace" "github-provisioner" {
+  name           = "github-provisioner"
+  organization   = data.tfe_organization.rwaltr.name
+  execution_mode = "local"
+}
+
+resource "tfe_workspace" "doppler-provisioner" {
+  name           = "doppler-provisioner"
+  organization   = data.tfe_organization.rwaltr.name
+  execution_mode = "local"
+}
