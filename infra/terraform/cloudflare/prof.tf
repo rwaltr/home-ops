0adc1dd94f749c708ab2aeefa6cc39e81673b81d
Ryan Walter <rwalt@pm.me>
2023-07-10 19:20:25 -0500

    new domain
    Signed-off-by: Ryan Walter <rwalt@pm.me>

---
+++ infra/terraform/cloudflare/prof.tf
@@ -0,0 +1,43 @@
+resource "cloudflare_zone" "prof_domain" {
+  zone = "rwalt.pro"
+  account_id = cloudflare_account.waltrtech.id
+}
+
+resource "cloudflare_zone_settings_override" "prof_domain" {
+  zone_id = cloudflare_zone.prof_domain.id
+  settings {
+    ssl                      = "strict"
+    always_use_https         = "on"
+    min_tls_version          = "1.2"
+    opportunistic_encryption = "on"
+    tls_1_3                  = "zrt"
+    automatic_https_rewrites = "on"
+    universal_ssl            = "on"
+    browser_check            = "on"
+    challenge_ttl            = 1800
+    privacy_pass             = "on"
+    security_level           = "medium"
+    brotli                   = "on"
+    minify {
+      css  = "on"
+      js   = "on"
+      html = "on"
+    }
+    rocket_loader       = "off"
+    always_online       = "off"
+    development_mode    = "off"
+    http3               = "on"
+    zero_rtt            = "on"
+    ipv6                = "on"
+    websockets          = "on"
+    opportunistic_onion = "on"
+    pseudo_ipv4         = "off"
+    ip_geolocation      = "on"
+    email_obfuscation   = "on"
+    server_side_exclude = "on"
+    hotlink_protection  = "off"
+    security_header {
+      enabled = false
+    }
+  }
+}
