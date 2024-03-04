3e484963150d8865f2a6e7ff297768926e69d389
Ryan Walter <rwalt@pm.me>
2024-03-04 00:14:53 -0600

    lolwat

--- infra/nix/hosts/mouse/default.nix
+++ infra/nix/hosts/mouse/default.nix
@@ -61,6 +61,9 @@
   ];
 
 
+  # TODO: Switch to prometheus when prometheus
+  services.netdata.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
